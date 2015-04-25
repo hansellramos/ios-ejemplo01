cf66ad7e15fd51a350903ea60a19003f522949e8
hansellesteban <hansell.ramos@gmail.com>
2015-04-25 10:57:40 -0500

    Initial Commit

---
+++ Ejemplo01/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Ejemplo01
+//
+//  Created by Hansel Ramos Osorio on 4/25/15.
+//  Copyright (c) 2015 Hansel Ramos Osorio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
